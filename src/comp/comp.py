f56faae65306f805a92f2531310f8c2e1ec6a94c
Derek <legato_5@yahoo.com>
2020-07-19 16:05:19 -0700

    filled out first portion of sprint challenge

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -25,29 +25,43 @@ humans = [
 # whose name starts with 'D':
 print("Starts with D:")
 a = []
+for human in humans:
+    if human.name[0] == "D":
+        a.append(human)
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
 b = []
+for human in humans:
+    if human.name[len(human.name)-1] == "e":
+        b.append(human)
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
 c = []
+for human in humans:
+    if human.name[0] >= "C" and human.name[0] <= "G":
+        c.append(human)
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
 d = []
+for human in humans:
+    human.age += 10
+    d.append(human)
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
 e = []
+for human in humans:
+    e.append(f"{human.name}-{human.age}")
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
@@ -55,6 +69,8 @@ print(e)
 # inclusive.
 print("Names and ages between 27 and 32:")
 f = []
+for human in humans:
+    f.append(tuple((f"{human.name}", human.age)))
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
@@ -62,10 +78,14 @@ print(f)
 # The "humans" list should be unmodified.
 print("All names uppercase:")
 g = []
+for human in humans:
+    g.append(Human(human.name.upper(), human.age + 5))
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
 h = []
+for human in humans:
+    h.append(math.sqrt(human.age))
 print(h)
